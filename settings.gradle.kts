19b7598438ac1fe5e8f660e0140234ebe029b5fa
Trịnh Anh Tuấn <trinhanhtuan20112003@gmail.com>
2025-02-14 21:19:46 +0700

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        jcenter()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        jcenter()
+        maven { setUrl("https://jitpack.io") }
+    }
+}
+
+rootProject.name = "BloodBankNauT"
+include(":app")
+ 
